78c095c2acdcc7db8c8dabb3bcb4466c42f0933e
chriskimty <christaeyoung.kim@gmail.com>
2023-01-07 13:34:58 -0500

    added routes with react-router-dom

--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,8 @@ import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-    // useFetch custom hook here
-    // destructuring of the info from useFetch
-    // note data: blogs allow us to call it a different name within this component
     const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
-    // ^all we have to do is call the custom hook and pass the argument which was the url
+    
     return (
         <div className="home">
             {error && <div>{error}</div>}
