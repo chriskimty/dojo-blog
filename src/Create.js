93deb3efc3f5702d4fcd167249e24ceeabdce8e6
chriskimty <christaeyoung.kim@gmail.com>
2023-01-07 14:54:11 -0500

    making a POST request

--- src/Create.js
+++ src/Create.js
@@ -5,10 +5,32 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
+    const [isPending, setIsPending] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // create blog object which will save the data
+        const blog = { title, body, author }
+        // console.log(blog);
+        setIsPending(true);
+    // Then we need to make a POST request
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body:JSON.stringify(blog)
+        }).then(() => {
+            console.log('new blog added')
+            setIsPending(false);
+        })
+        setTitle('');
+        setBody('');
+        setAuthor('mario');
+    }
+
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog title:</label>
                 <input
                     type="text"
@@ -30,7 +52,12 @@ const Create = () => {
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
-                <button>Add Blog</button>
+                {/* {!isPending && <button>Add Blog</button>}
+                { isPending && <button disabled>Adding blog...</button>} */}
+                {/* cleaner way is to use ternary */}
+                {!isPending
+                    ? <button>Add Blog</button>
+                    : <button disabled>Adding blog...</button>}
             </form>
         </div>
     )
