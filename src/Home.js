07957ad1bf70ff5706f3c92af1b51b73f6994156
chriskimty <christaeyoung.kim@gmail.com>
2023-01-07 13:05:40 -0500

    fetched data and conditional templating to display data

--- src/Home.js
+++ src/Home.js
@@ -2,32 +2,28 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
+    const [blogs, setBlogs] = useState(null)
 
-    const [name, setName] = useState('mario')
+    // const handleDelete = (id) => {
+    //     const newBlogs = blogs.filter(blog => blog.id !== id);
+    //     setBlogs(newBlogs)
+    // }
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs)
-    }
-
-    // useEffect: runs a function every render of the component (i.e. when the state changes)
-    // add dependency array if you only want to run the func once on first render
     useEffect(() => {
-        console.log('it ran')
-    },[name])
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+            return res.json()
+            })
+            .then((data) => {
+                // console.log(data)
+                setBlogs(data)
+        })
+    },[])
     
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-            <button onClick={() => setName(
-                'luigi'
-            )}>change name</button>
-            <p>{name}</p>
+            {/* conditional templating - evaluates left first, if it's false, it doesn't bother with the right  */}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
     );
 }
