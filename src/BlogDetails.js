3575eaa249cf3e9afe0177339610571520fff29e
chriskimty <christaeyoung.kim@gmail.com>
2023-01-07 14:05:00 -0500

    reusing custom hooks

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,21 @@
-// Route parameters - this will show this component for whatever comes after /blogs/XXX 
 import { useParams } from "react-router-dom";
+import useFetch from './useFetch';
 
 const BlogDetails = () => {
     const { id } = useParams();
+    const {data: blog, error, isPending} = useFetch('http://localhost:8000/blogs/' + id )
 
     return (
         <div className="blog-details">
-            <h2>Blog Details - {id}</h2>
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            )}
         </div>
     )
 }
