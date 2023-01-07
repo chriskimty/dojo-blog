f85881ad978da0e2e1c2568897332eac4020d999
chriskimty <christaeyoung.kim@gmail.com>
2023-01-07 01:48:44 -0500

    useEffect dependencies

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -8,16 +8,26 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
 
+    const [name, setName] = useState('mario')
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs)
     }
+
+    // useEffect: runs a function every render of the component (i.e. when the state changes)
+    // add dependency array if you only want to run the func once on first render
+    useEffect(() => {
+        console.log('it ran')
+    },[name])
     
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-            {/* we'll take the above object and only include the ones that have the author of mario */}
-            {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs!"/> */}
+            <button onClick={() => setName(
+                'luigi'
+            )}>change name</button>
+            <p>{name}</p>
         </div>
     );
 }
