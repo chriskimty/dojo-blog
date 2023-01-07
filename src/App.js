357904355d0c12c790cc73845b99b8302ae45fe7
chriskimty <christaeyoung.kim@gmail.com>
2023-01-07 00:50:25 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  // Numbers, strings, arrays can be displayed dynamically (they're all converted to strings)
+  const title = 'Welcome to the new blog'
+  const likes = 50;
+  // Cannot output booleans or objects
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{ title }</h1>
+        <p>Liked {likes} times</p>
+        
+        <p> {10}</p>
+        <p> {'hello cats'}</p>
+        <p> {[1, 2, 3, 4, 5]}</p>
+        <p>{ Math.random() * 10 }</p>
+     </div>
     </div>
   );
 }
